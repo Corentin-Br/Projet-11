ca4ff13cf3eecba566a9d6cbf7bf83a5e079da3f
Corentin BRAVO <corentinbravo@gmail.com>
2021-08-27 11:01:12 +0200

    formatted file to fit PEP.

--- server.py
+++ server.py
@@ -3,25 +3,25 @@ from flask import Flask, render_template, request, redirect, flash, url_for
 import datetime
 
 
-def loadClubs():
+def load_clubs():
     with open('clubs.json') as c:
-         listOfClubs = json.load(c)['clubs']
-         return listOfClubs
+        all_clubs = json.load(c)['clubs']
+        return all_clubs
 
 
-def loadCompetitions():
+def load_competitions():
     with open('competitions.json') as comps:
-         listOfCompetitions = json.load(comps)['competitions']
-         for competition in listOfCompetitions:
-             competition["places_taken"] = {}
-         return listOfCompetitions
+        all_competitions = json.load(comps)['competitions']
+        for competition in all_competitions:
+            competition["places_taken"] = {}
+        return all_competitions
 
 
 app = Flask(__name__)
 app.secret_key = 'something_special'
 
-competitions = loadCompetitions()
-clubs = loadClubs()
+competitions = load_competitions()
+clubs = load_clubs()
 DATE_PATTERN = "%Y-%m-%d %H:%M:%S"
 
 
@@ -30,12 +30,12 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/showSummary',methods=['POST'])
-def showSummary():
-    potential_clubs= [club for club in clubs if club['email'] == request.form['email']]
+@app.route('/showSummary', methods=['POST'])
+def show_summary():
+    potential_clubs = [club for club in clubs if club['email'] == request.form['email']]
     if potential_clubs:
         club = potential_clubs[0]
-        return render_template('welcome.html',club=club,competitions=competitions)
+        return render_template('welcome.html', club=club, competitions=competitions)
     else:
         flash("Your email has not been found.")
         return render_template('index.html'), 404
@@ -47,33 +47,33 @@ def show_points():
 
 
 @app.route('/book/<competition>/<club>')
-def book(competition,club):
-    foundClubs = [c for c in clubs if c['name'] == club]
-    foundCompetitions = [c for c in competitions if c['name'] == competition]
-    if foundClubs and foundCompetitions:
-        return render_template('booking.html',club=foundClubs[0],competition=foundCompetitions[0])
+def book(competition, club):
+    found_clubs = [c for c in clubs if c['name'] == club]
+    found_competitions = [c for c in competitions if c['name'] == competition]
+    if found_clubs and found_competitions:
+        return render_template('booking.html', club=found_clubs[0], competition=found_competitions[0])
     else:
         flash("Something went wrong-please try again")
         return render_template('welcome.html', club=club, competitions=competitions), 500
 
 
-@app.route('/purchasePlaces',methods=['POST'])
-def purchasePlaces():
+@app.route('/purchasePlaces', methods=['POST'])
+def purchase_places():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
-    placesRequired = int(request.form['places'])
+    required_places = int(request.form['places'])
     already_taken_places = competition["places_taken"].get(club["name"], 0)
-    message, allowed = can_buy_places(club, competition, placesRequired, already_taken_places)
+    message, allowed = can_buy_places(club, competition, required_places, already_taken_places)
     status = 200 if allowed else 500
-    if allowed :
-        competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-        competition["places_taken"][club["name"]] = already_taken_places + placesRequired
-        club["points"] = int(club['points']) - 3 * placesRequired
+    if allowed:
+        competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-required_places
+        competition["places_taken"][club["name"]] = already_taken_places + required_places
+        club["points"] = int(club['points']) - 3 * required_places
     flash(message)
     return render_template('welcome.html', club=club, competitions=competitions), status
 
 
-def can_buy_places(club: dict, competition: dict, required_places: int, already_taken_places:int) -> tuple:
+def can_buy_places(club: dict, competition: dict, required_places: int, already_taken_places: int) -> tuple:
     if datetime.datetime.now() >= datetime.datetime.strptime(competition["date"], DATE_PATTERN):
         return 'You cannot reserve a competition that already took place!', False
     if required_places > 12 - already_taken_places:
